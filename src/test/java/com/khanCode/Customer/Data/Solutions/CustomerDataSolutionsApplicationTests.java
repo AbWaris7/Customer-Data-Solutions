6a1bfeb2cebe010f975a7402c891208f690a77fb
abdulwariskhan <hellokhan.ch@gmail.com>
2025-01-08 14:56:23 +0100

    Initial commit

---
+++ src/test/java/com/khanCode/Customer/Data/Solutions/CustomerDataSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khanCode.Customer.Data.Solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerDataSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
